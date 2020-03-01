7d64f054882942a3ae289844ec8b61413aa37b81
hyori100 <ag497752@naver.com>
2020-03-01 12:46:23 +0900

    화면 재구성

--- eyegotit/app/src/main/java/com/sangmee/eyegottttt/Map/InformationActivity.java
+++ eyegotit/app/src/main/java/com/sangmee/eyegottttt/Map/InformationActivity.java
@@ -2,14 +2,27 @@ package com.sangmee.eyegottttt.Map;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.sangmee.eyegottttt.R;
 
 public class InformationActivity extends AppCompatActivity {
     private String user_id;
     private Intent intent;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private String getUser_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +38,19 @@ public class InformationActivity extends AppCompatActivity {
         TextView userid_TextView = findViewById(R.id.userid_textview);
         userid_TextView.setText(user_id);
 
+        Query recentPostsQuery = databaseReference.child(user_id).child("user_id");
+        recentPostsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                getUser_id= dataSnapshot.getValue().toString();
+                TextView getTextView=findViewById(R.id.userid_textview4);
+                getTextView.setText(getUser_id);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
